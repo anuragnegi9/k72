24f07865059fb4d5e63a60c933e7361156696206
anurag_negi9 <anruagnegi7455@gmail.com>
2026-01-28 21:43:57 +0530

    Fix homepage video for Vercel deploy

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-      <video className='h-full w-full object-cover' autoPlay muted loop src='../../../public/HeroVideo.mp4' />
+      <video className='h-full w-full object-cover' autoPlay muted loop src='/HeroVideo.mp4' />
     </div>
   )
 }
